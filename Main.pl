b2775de1c314b0286df8cd91ccfa2c38dcadefb9
sctnightcore <nightcoremax@gmail.com>
2019-03-20 23:54:56 +0700

    Update Main.pl

--- Main.pl
+++ Main.pl
@@ -8,13 +8,15 @@ use TrueWallet qw(getToken getProfile getBalance getActivity topupTW txDetail lo
 Main();
 sub Main {
 	# login to TrueWallet
-	my $token = getToken('username', 'password');
+	my $email = '';
+	my $password = '';
+	my $token = getToken($email, $password);
 	if (defined $token) {
 		# check Balance 
 		my $balance = getBalance($token);
 		print "You have balance is $balance\n";
 		# check Activity [Start - end] // check all Report id 
-		my $activity = getActivity($token,'2018-12-28','2019-01-01'); #start , end
+		my $activity = getActivity($token,'2018-12-28','2019-01-02'); #start , end [YYYY-MM-DD] 
 		foreach my $i ( @$activity ) {
    			my $txdetail = txDetail($token, $i->{report_id});
 			print "======================================\n";
