9f1f2f42fb9340d0005fce52e1cb4a56a4c51fe5
sctnightcore <nightcoremax@gmail.com>
2019-03-20 23:01:56 +0700

    Update Main.pl

--- Main.pl
+++ Main.pl
@@ -12,23 +12,18 @@ sub Main {
 		# check Balance 
 		my $balance = TrueWallet::getBalance($token);
 		print "You have balance is $balance\n";
-		# check Activity [Start - end]
+		# check Activity [Start - end] // check all Report id 
 		my $activity = TrueWallet::getActivity($token,'2018-12-28','2019-01-01'); #start , end
 		foreach my $i ( @$activity ) {
+   			my $txdetail = TrueWallet::txDetail($token, $i->{report_id});
 			print "======================================\n";
 			printf "Report ID: %s\n", $i->{report_id};
 		    printf "Date: %s\n", $i->{date_time};
-		    printf "Original Type: %s\n", $i->{original_type};
-		    printf "Amount: %s\n", $i->{amount};
+		    printf "Topup Mode: %s\n",$txdetail->{service_type};
+		    printf "To: %s\n", $txdetail->{ref1};
+   		    printf "Amount: %s\n", $i->{amount};
 		    print "======================================\n";
 		}
-		#check Activity with Report_id
-		my $txdetail = TrueWallet::txDetail($token,'549939742'); #549939742 = $activity->{report_id}
-		print "====================\n";
-		printf "Report ID: %s\n",'549939742';
-		printf "Topup Mode: %s\n",$txdetail->{service_type};
-		printf "To: %s\n",$txdetail->{ref1};
-		print "====================\n";
 		#logout from TrueWallet
 		my $logout = TrueWallet::logout($token);
 	} else {
